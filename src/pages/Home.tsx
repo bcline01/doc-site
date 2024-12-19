522c16547cd87b4ac3b821720dc31c94b4c760b1
bcline01 <brookecline.bc@gmail.com>
2024-12-19 12:15:31 -0500

    fixed some responsivness in appointment and services tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -31,16 +31,16 @@ const Home: React.FC = () => {
       </ul>
     </nav>
 
-    <section id="section1" className="home-page">
-    <video autoPlay loop muted className="background-video">
-    <source src={Video} type="video/mp4" />
-    Your browser does not support the video tag.
-    </video>
-    <div className="content">
-    
-      <p style={{color:'black', fontWeight:'600px', backgroundColor:'white', opacity:'0.4', borderRadius:'6px', padding:'10px'}}>We Welcome New Patients!<button className='home-btn'>Request a New Appointment</button></p>
-    </div>
-  </section>
+      <section id="section1" className="home-page">
+        <video autoPlay loop muted className="background-video">
+          <source src={Video} type="video/mp4" />
+          Your browser does not support the video tag.
+        </video>
+        <div className="content">
+
+          <p className='welcome'>We Welcome New Patients!<button className='home-btn'>Request a New Appointment</button></p>
+        </div>
+      </section>
 
   <section id="section2" className="review-page">
     <Review />
