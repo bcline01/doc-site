bd61798a7ec6c79230b76cf508fb08c0812ea112
bcline01 <brookecline.bc@gmail.com>
2025-01-29 11:09:56 -0500

    created spinal page

--- src/pages/Spinal.tsx
+++ src/pages/Spinal.tsx
@@ -1,14 +1,53 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import FirstVisit from '../components/FirstVisit';
+import Footer from '../components/Footer';
+import Navbar from '../components/Navbar';
+import SpinalPic from '../assets/spinal.png';
 
 const Spinal: React.FC = () => {
+  const [loaded, setLoaded] = useState(false);
+  useEffect(() => {
+    const timer = setTimeout(() => setLoaded(true), 100); // Add a slight delay to trigger animations
+    return () => clearTimeout(timer); // Cleanup timeout
+  }, []);
+
+
   return (
     <div>
-      <h1>Spinal Decompression</h1>
-      <p>
-        Detailed information about Chiropractic Care goes here. Include benefits, process, and any other details you'd like to display.
-      </p>
+      <div>
+        <Navbar />
+      </div>
+      <div className='laser-page'>
+    <div className="laser-container">
+      <div className={`laser-image ${loaded ? 'fade-in' : ''}`} >
+        <img src={SpinalPic} alt="Laser Therapy" />
+      </div>
+      <div className={`laser-content ${loaded ? 'fade-in' : ''}`}>
+        <h1>
+          Spinal Decompression
+        </h1>
+        <p><>
+        Spinal decompression therapy is a non-invasive treatment that gently stretches the spine to relieve pressure on the spinal discs, nerves, and other structures in the back. This process aims to{' '} 
+        <span className='italic'>promote healing, reduce pain, and improve mobility.</span> It is commonly used to treat conditions like herniated discs, sciatica, and degenerative disc disease.</></p>
+        <ul>
+          <li>Helps relieve pain associated with conditions like bulging or herniated discs.</li>
+          <li>By creating negative pressure, it encourages the flow of nutrients and oxygen into the affected area, speeding up recovery.</li>
+          <li> Helps restore proper alignment and enhances movement, reducing stiffness and discomfort.</li>
+          <li>Can help reduce swelling around nerves and tissues, leading to less pain.</li>
+        </ul>
+      </div>
+    </div>
+    
+    <div>
+      <FirstVisit />
+      </div>
+      </div>
+      <div>
+        <Footer />
+      </div>
     </div>
   );
 };
+
 
 export default Spinal;
