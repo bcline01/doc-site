5c7d7a53500d54c773924abc8f5d9074bf8ffdb0
bcline01 <brookecline.bc@gmail.com>
2025-01-30 15:43:07 -0500

    form is styled

--- src/pages/Form.tsx
+++ src/pages/Form.tsx
@@ -1,32 +1,37 @@
 import React from 'react';
+import Navbar from '../components/Navbar';
 
 const Form: React.FC = () => {
   return (
-    <div style={{ 
-      textAlign: "center", 
-      backgroundColor: "rgb(236, 232, 232)",  // Light blue background
-      padding: "20px", 
-      minHeight: "100vh" 
-    }}>
-      {/* <h2 style={{fontFamily:'raleway', letterSpacing:'2px'}}>New Patient Appointment</h2> */}
-      <iframe
-    src="https://docs.google.com/forms/d/e/1FAIpQLSdcic3nS2-pk8BVZEkKj-f6hvLn7_cTiwedqDYMFqgh2ltneg/viewform?embedded=true"
-    width="100%" // Makes the iframe take up full width
-    height="100%" // Makes the iframe take up full height relative to its parent
-    // frameBorder="0"
-    // marginHeight="0"
-    // marginWidth="0"
-    title="New Patient Appointment Form"
-    style={{
-      position: "absolute",
-      top: "0",
-      left: "0",
-      bottom: "0",
-      right: "0",
-    }}
-  >
-        Loading…
-      </iframe>
+    <div>
+      <Navbar />
+      <div
+        style={{
+          textAlign: 'center',
+          backgroundColor: 'rgb(236, 232, 232)', // Light blue background
+          padding: '20px',
+          minHeight: '100vh',
+          marginTop: '80px', // Push content below navbar
+          position: 'relative', // Allow iframe positioning to be relative to this container
+        }}
+      >
+        {/* <h2 style={{fontFamily:'raleway', letterSpacing:'2px'}}>New Patient Appointment</h2> */}
+        <iframe
+          src="https://docs.google.com/forms/d/e/1FAIpQLSdcic3nS2-pk8BVZEkKj-f6hvLn7_cTiwedqDYMFqgh2ltneg/viewform?embedded=true"
+          width="100%" // Makes the iframe take up full width
+          height="100%" // Makes the iframe take up full height relative to its parent
+          style={{
+            position: 'absolute',
+            top: '0', // position relative to the container
+            left: '0',
+            bottom: '0',
+            right: '0',
+          }}
+          title="New Patient Appointment Form"
+        >
+          Loading…
+        </iframe>
+      </div>
     </div>
   );
 };
